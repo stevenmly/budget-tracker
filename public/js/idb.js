998513cd352a732f1f685266cb4b3caafa870c5e
Steven Ly <ly.steven16@gmail.com>
2021-07-20 04:09:14 -0500

    Changes to idb.js to fix bug where new entries wont diplay on page

--- public/js/idb.js
+++ public/js/idb.js
@@ -6,14 +6,14 @@ request.onupgradeneeded = function (event) {
 	// save a reference to the database 
 	const db = event.target.result;
 	// create an object store (table) called `new_transaction`, set it to have an auto incrementing primary key of sorts
-	db.createObjectStore("new_transaction", { autoIncrement: true });
+	db.createObjectStore("new_record", { autoIncrement: true });
 };
 
 request.onsuccess = function (event) {
 	db = event.target.result;
 
 	if (navigator.onLine) {
-		uploadTransaction();
+		checkDatabase();
 	}
 };
 
@@ -22,49 +22,54 @@ request.onerror = function (event) {
 };
 
 function saveRecord(record) {
-	const transaction = db.transaction(["new_transaction"], "readwrite");
+	const transaction = db.transaction(["new_record"], "readwrite");
 
-	const transactionObjectStore = transaction.objectStore("new_transaction");
+	const budgetObjectStore = transaction.objectStore("new_record");
 
-	transactionObjectStore.add(record);
+	budgetObjectStore.add(record);
 }
 
-function uploadTransaction() {
-	const transaction = db.transaction(["new_transaction"], "readwrite");
-
-	const transactionObjectStore = transaction.objectStore("new_transaction");
-
-	// get all records from store and set to a variable
-	const getAll = transactionObjectStore.getAll();
-
-	getAll.onsuccess = function () {
-		// if there was data in indexedDb's store, let's send it to the api server
-		if (getAll.result.length > 0) {
-			fetch("/api/transaction", {
-				method: "POST",
-				body: JSON.stringify(getAll.result),
-				headers: {
-					Accept: "application/json, text/plain, */*",
-					"Content-Type": "application/json",
-				},
-			})
-				.then((response) => response.json())
-				.then((serverResponse) => {
-					if (serverResponse.message) {
-						throw new Error(serverResponse);
-					}
-
-					const transaction = db.transaction(["new_transaction"], "readwrite");
-					const transactionObjectStore = transaction.objectStore("new_transaction");
-					transactionObjectStore.clear();
-				})
-				.catch((err) => {
-					// set reference to redirect back here
-					console.log(err);
-				});
-		}
-	};
-}
+function checkDatabase() {
+    // open a transaction on your db
+    const transaction = db.transaction(['new_record'], 'readwrite');
+  
+    // access your object store
+    const budgetObjectStore = transaction.objectStore('new_record');
+  
+    // get all records from store and set to a variable
+    const getAll = budgetObjectStore.getAll();
+  
+    getAll.onsuccess = function() {
+        // if there was data in indexedDb's store, let's send it to the api server
+        if (getAll.result.length > 0) {
+          fetch('/api/transaction/bulk', {
+            method: 'POST',
+            body: JSON.stringify(getAll.result),
+            headers: {
+              Accept: 'application/json, text/plain, */*',
+              'Content-Type': 'application/json'
+            }
+          })
+            .then(response => response.json())
+            .then(serverResponse => {
+              if (serverResponse.message) {
+                throw new Error(serverResponse);
+              }
+              // open one more transaction
+              const transaction = db.transaction(['new_record'], 'readwrite');
+              // access the new_record object store
+              const budgetObjectStore = transaction.objectStore('new_record');
+              // clear all items in your store
+              budgetObjectStore.clear();
+    
+              alert('All saved data has been submitted!');
+            })
+            .catch(err => {
+              console.log(err);
+            });
+        }
+      };
+  }
 
 // listen for app coming back online
 window.addEventListener("online", uploadTransaction);
